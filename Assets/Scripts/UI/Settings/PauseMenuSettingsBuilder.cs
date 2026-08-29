328fed885d5e583e191736606d6acc2c9e9d238e
Mykhailo Ostrovskyi <murasama.off@gmail.com>
2026-08-29 18:50:06 +0200

    refactor(ui): extract PauseMenuDebugSectionBuilder from PauseMenuSettingsBuilder

--- Assets/Scripts/UI/Settings/PauseMenuSettingsBuilder.cs
+++ Assets/Scripts/UI/Settings/PauseMenuSettingsBuilder.cs
@@ -1159,67 +1159,9 @@ namespace Fodinae.UI
         /// </summary>
         public Foldout BuildDebugSection()
         {
-            var debugSection = new Foldout
-            {
-                text = _loc.Get("settings.debug.tools"),
-                value = false,
-            };
-            debugSection.AddToClassList("settings-section");
-            debugSection.AddToClassList("settings-section--debug");
-            _debugSection = debugSection;
-
-            debugSection.Add(PauseMenuUIFactory.CreateLabel(_loc.Get("settings.debug.tools")));
-            debugSection.Add(PauseMenuUIFactory.CreateButton(_loc.Get("settings.debug.test_kick"), () =>
-            {
-                _connectionService.TriggerDisconnect(_loc.Get("settings.debug.test_disconnect"));
-                _closeMenu();
-            }));
-            debugSection.Add(PauseMenuUIFactory.CreateButton(_loc.Get("settings.debug.test_reconnect"), () =>
-            {
-                _connectionService.TriggerReconnect(_loc.Get("settings.debug.server_restart"));
-                _closeMenu();
-            }));
-            debugSection.Add(PauseMenuUIFactory.CreateButton(_loc.Get("settings.debug.test_open_url"), () =>
-            {
-                SendElementClick("open_url_test");
-            }));
-            debugSection.Add(PauseMenuUIFactory.CreateButton(_loc.Get("settings.debug.test_modal"), () =>
-            {
-                SendElementClick("test_modal");
-            }));
-            debugSection.Add(PauseMenuUIFactory.CreateButton(_loc.Get("settings.debug.join_clan"), () =>
-            {
-                SendElementClick("join_clan");
-            }));
-            debugSection.Add(PauseMenuUIFactory.CreateButton(_loc.Get("settings.debug.leave_clan"), () =>
-            {
-                SendElementClick("leave_clan");
-            }));
-            debugSection.Add(PauseMenuUIFactory.CreateButton(_loc.Get("settings.debug.test_mission_arrow"), () =>
-            {
-                SendElementClick("test_mission_arrow");
-            }));
-            debugSection.Add(PauseMenuUIFactory.CreateButton(_loc.Get("settings.debug.missions"), () =>
-            {
-                SendElementClick("open_missions");
-            }));
-            debugSection.Add(PauseMenuUIFactory.CreateButton(_loc.Get("settings.debug.walls_off"), () =>
-            {
-                ILocalPlayer? player = _localPlayer.Current;
-                if (player != null)
-                {
-                    player.IgnoreCollision = !player.IgnoreCollision;
-                    _closeMenu();
-                }
-            }));
-
-            return debugSection;
-        }
-
-        private void SendElementClick(string tag)
-        {
-            _networkService.Send(new ElementClickPacket(tag, 0, Array.Empty<StringPairPacket>()));
-            _closeMenu();
+            var builder = new PauseMenuDebugSectionBuilder(_networkService, _connectionService, _localPlayer, _closeMenu, _loc);
+            _debugSection = builder.Build();
+            return _debugSection;
         }
 
         private void AddLightingDebugControls()
